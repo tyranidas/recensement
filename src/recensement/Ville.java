b2554b1ac9034aaef216813af6f143961d690acd
Manuel <tyranidas@hotmail.com>
2022-10-20 17:49:48 +0200

    recensement 3 4 et 5

--- src/recensement/Ville.java
+++ src/recensement/Ville.java
@@ -2,8 +2,62 @@ package recensement;
 
 public class Ville {
 
-	public Ville() {
-		// TODO Auto-generated constructor stub
+	String regCode;
+	String regName;
+	String depCode;
+	String comCode;
+	String comName;
+	int comPop;
+	public Ville(String regCode, String regName, String depCode, String comCode, String comName, int comPop) {
+		this.regCode = regCode;
+		this.regName = regName;
+		this.depCode = depCode;
+		this.comCode = comCode;
+		this.comName = comName;
+		this.comPop = comPop;
 	}
+	
+	public String toString()
+	{
+		return "La ville "+getComName();
+	}
+	public String getRegCode() {
+		return regCode;
+	}
+	public void setRegCode(String regCode) {
+		this.regCode = regCode;
+	}
+	public String getRegName() {
+		return regName;
+	}
+	public void setRegName(String regName) {
+		this.regName = regName;
+	}
+	public String getDepCode() {
+		return depCode;
+	}
+	public void setDepCode(String depCode) {
+		this.depCode = depCode;
+	}
+	public String getComCode() {
+		return comCode;
+	}
+	public void setComCode(String comCode) {
+		this.comCode = comCode;
+	}
+	public String getComName() {
+		return comName;
+	}
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+	public int getComPop() {
+		return comPop;
+	}
+	public void setComPop(int comPop) {
+		this.comPop = comPop;
+	}
+	
+	
 
 }
